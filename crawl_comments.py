e4ec2bc3f35dbe5bc72bce7e5fb2b53efc6d12d3
parasol <wutongit1995@outlook.com>
2021-12-26 01:14:32 +0800

    [feat] add scripts for douban

---
+++ crawl_comments.py
@@ -0,0 +1,181 @@
+import re
+import requests
+import random
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import urllib
+HEADER={
+    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36',
+#     'Referer': 'https://accounts.douban.com/passport/login_popup?login_source=anony',
+#     'Origin': 'https://accounts.douban.com',
+#     'content-Type':'application/x-www-form-urlencoded',
+#     'x-requested-with':'XMLHttpRequest',
+#     'accept':'application/json',
+#     'accept-encoding':'gzip, deflate, br',
+#     'accept-language':'zh-CN,zh;q=0.9',
+#     'connection': 'keep-alive',
+ }
+
+# find your cookies in the douban website
+COOKIES = {
+    'cookie': 
+}
+
+class CrawlComments:
+
+    def __init__(self, url_temp, login_name, login_password, dump2csv=False, dump_file_name=None):
+        self.url_temp = url_temp
+        self.login_name = login_name
+        self.login_password = login_password
+        self.dump2csv = dump2csv
+        self.dump_file_name = dump_file_name
+        self.res = {
+            "contents": [],
+            "rates": [],
+            "times": []
+        }
+        self.cookies = None
+        self.login_url = 'https://accounts.douban.com/j/mobile/login/basic'
+        self._get_cookies()
+
+
+    def _get_cookies(self):
+        data = {
+            'ck':'',
+            'name': self.login_name,
+            'password': self.login_password,
+            'remember':'false',
+            'ticket':''
+        }
+        data = urllib.parse.urlencode(data)
+        req = requests.post(url, headers=HEADER, data=data, verify=False)
+        cookies = requests.utils.dict_from_cookiejar(req.cookies)
+        self.cookies = cookies
+        self.cookies = COOKIES
+        print(cookies)
+
+    def _crawl_html(self, url):
+        res = requests.get(url, headers=HEADER, cookies=self.cookies)
+        return res
+
+    def _parse_html(self, string):
+        return BeautifulSoup(string, "lxml")
+
+    def _get_all_time(self, soup):
+        res = soup.find_all("span", attrs={"class": "comment-time"})
+        res = list(map(lambda x: x.text.strip(), res))
+        return res
+
+    def _get_all_content(self, soup):
+        res = soup.find_all(attrs={"class": "comment-content"})
+        res = list(map(lambda x: x.text.strip(), res))
+        return res
+
+    def _get_all_rates(self, soup):
+        res = soup.find_all(class_=re.compile("allstar.*rating"))
+        res = list(map(lambda x: x.attrs["class"][0].strip()[-2], res))
+        return res
+    
+    def get_comment_info(self, soup):
+        items = soup.select('.comment-item')
+        times, rates, contents = [], [], []
+
+        for item in items:
+            try:
+                time = item.select(".comment-time")[0].text.strip()
+                time = time.replace('\n', ' ')
+            except Exception as e:
+                print("time", e)
+                time = "1970:01:01 00:00:00"
+            
+            try:
+                content = item.select(".short")[0].text.strip()
+                content = content.replace('\n', ' ')
+            except Exception as e:
+                print("content", e)
+                content = ""
+            
+            try:
+                rate = item.select(".comment-info")[0].select('span')[1].attrs['class'][0][-2]
+                rate = rate.replace('\n', '')
+            except Exception as e:
+                rate = ""
+                print("rate", e)
+            
+            if content == "" and rate == "":
+                continue
+
+            times.append(time)
+            rates.append(rate)
+            contents.append(content)
+            
+        return times, rates, contents
+
+    def craw_time_rate_context(self, soup):
+        times = self._get_all_time(soup)
+        rates = self._get_all_rates(soup)
+        contents = self._get_all_content(soup)
+        
+        return times, rates, contents
+
+    def craw_single_page(self, url):
+        response = self._crawl_html(url)
+        s_code = response.status_code
+        if s_code != 200:
+            print("status_code: %d, url %s is in trouble, skip this" % (s_code, url))
+            return -3
+        
+        soup = self._parse_html(response.text)
+        self.soup = soup
+        times, rates, contents = self.get_comment_info(soup)
+
+        if len(times) == 0 and len(rates) == 0 and len(contents) == 0:
+            print("status code: %d, no more data" % (-2))
+            return -2
+
+        if len(times) != len(rates) or len(times) != len(contents):
+            print("status code: %d, times: %d, rates: %d, contents: %d, not equal, \n the url is %s" % (-1, len(times), len(rates), len(contents), url))
+            return -1
+
+        self.res["times"] += times
+        self.res["rates"] += rates
+        self.res["contents"] += contents
+        return 0
+
+    def start_crawl(self):
+        print("======Start Crawling, plz wait======")
+
+        start = 0
+        while True:
+            try:
+                if start % 100 == 0:
+                    print("I have crawled %d comments. Your movie is so popular" % start)
+                time.sleep(random.randint(10, 20))
+                url = self.url_temp % start
+                status_code = self.craw_single_page(url)
+                if status_code == -2:
+                    break
+                start += 20
+            except KeyboardInterrupt:
+                break
+
+        if len(self.res['times']) == 0 and len(self.res['rates']) == 0 and len(self.res['contents']):
+            print('WARNING: There is no data for url, plz check your url\n')
+            return
+
+        df = pd.DataFrame.from_dict(self.res)
+        df = df[['times', 'rates', 'contents']]
+        if self.dump2csv:
+            dump_file_name = self.dump_file_name
+            if dump_file_name is None:
+                cur_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                dump_file_name = cur_time + '.csv'
+            df.to_csv(dump_file_name, index=None)
+        print("======Thx for your patience, I crawled %d data=====" % (df.shape[0]))
+
+if __name__ == "__main__":
+    url = "https://movie.douban.com/subject/30346880/comments?start=%d&limit=20&status=P&sort=new_score"
+    craw_spider = CrawlComments(url, 'your_account', 'your_password', True, "the_bankers_douban.csv")
+    craw_spider.start_crawl()
+
---
+++ data_analyse.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import numpy as np
+import jieba
+import matplotlib.pyplot as plt
+import collections
+from wordcloud import WordCloud
+import zhon.hanzi
+import string
+import re
+from snownlp import SnowNLP
+
+jieba.enable_paddle()
+
+class DataAnalyse:
+    def __init__(self, path):
+        self.path = path
+        self.res = None
+        self.rate2comment = None
+        self.rate2count = None
+        self._get_comment_info()
+        self.stop_words = []
+        self.punctuation = zhon.hanzi.punctuation + string.punctuation
+        self._load_stop_words()
+
+    def _load_stop_words(self, path="all_stopwords.txt"):
+        lines = open(path, 'r', encoding='utf-8').readlines()
+        for line in lines:
+            self.stop_words.append(line.strip())
+     
+    def _get_comment_info(self):
+        df = pd.read_csv(self.path, encoding='utf-8')
+        res = []
+        for index, row in df.iterrows():
+            time, rate, content = row['times'], row['rates'], row['contents']
+            try:
+                rate = int(rate)
+            except Exception as e:
+                continue
+            res.append((time, rate, content.strip()))
+        self.res = sorted(res, key=lambda x: x[1], reverse=True)
+        return
+    
+    def _filter(self, word):
+        word = word.strip()
+        word = re.sub("[%s]" % "".join(zhon.hanzi.punctuation + string.punctuation), "", word)
+        if word in self.stop_words or word in self.punctuation:
+            return True
+        if word == " " or word == "":
+            return True
+        return False
+    
+    def _parse_comments(self):
+        rate2comment = {}
+        rate2count = {}
+        for item in self.res:
+            rate = item[1]
+            rate2count[rate] = rate2count.get(rate, 0) + 1
+            if rate not in rate2comment:
+                rate2comment[rate] = []
+            comment = list(jieba.cut(item[2], use_paddle=True))
+            new_comment = []
+            for c in comment:
+                if self._filter(c):
+                    continue
+                new_comment.append(c)
+                
+            rate2comment[rate] += list(new_comment)
+            
+        self.rate2comment = rate2comment
+        self.rate2count = rate2count
+    
+    def analyse(self, rate_threshold, topk):
+        self._parse_comments()
+        self._plot_rate_bar()
+        self._parse_sentiments(rate_threshold)
+        self._draw_wordcloud(rate_threshold, topk)
+    
+    def _parse_sentiments(self, rate_threshold):
+        ge_thres_sentiment = [0, 0]
+        lt_thres_sentiment = [0, 0]
+        for rate in range(1, rate_threshold):
+            for word in self.rate2comment[rate]:
+                word = SnowNLP(word)
+                lt_thres_sentiment[0] += word.sentiments
+                lt_thres_sentiment[1] += 1
+        for rate in range(rate_threshold, 6):
+            for word in self.rate2comment[rate]:
+                word = SnowNLP(word)
+                ge_thres_sentiment[0] += word.sentiments
+                ge_thres_sentiment[1] += 1
+        print("avg sentiments when rate >= %d is %.4f" % (rate_threshold, ge_thres_sentiment[0] / ge_thres_sentiment[1]))
+        print("avg sentiments when rate < %d is %.4f" % (rate_threshold, lt_thres_sentiment[0] / lt_thres_sentiment[1]))
+    
+    def _plot_rate_bar(self):
+        rates = []
+        sum_rate = 0.0
+        sum_count = 0.0
+        for i in range(1, 6):
+            rates.append(self.rate2count[i])
+            sum_rate += i * self.rate2count[i]
+            sum_count += self.rate2count[i]
+
+        plt.title("rate-count distribution")
+        plt.xlabel("rate")
+        plt.ylabel("count")
+        plt.bar(range(1, 6), rates)
+        print("The avgerage rate is %.2f" % (sum_rate / sum_count))
+        
+    def _draw_wordcloud(self, rate_threshold, topk=30):
+        
+        def _generate_wc(d, file_name):
+            greater_wordcloud = WordCloud(background_color="white",width=1000, height=860, margin=2, font_path="/System/Library/fonts/PingFang.ttc").fit_words(d)
+            greater_wordcloud.to_file(file_name)
+
+        def _filter_same_content(gt, lt):
+            new_gt = {}
+            new_lt = {}
+            for key in gt.keys():
+                if key not in lt.keys():
+                    new_gt[key] = gt[key]
+            for key in lt.keys():
+                if key not in gt.keys():
+                    new_lt[key] = lt[key]
+            return new_gt, new_lt
+
+        greater = []
+        lower = []
+        for k, v in self.rate2comment.items():
+            if k >= rate_threshold:
+                greater += v
+            else:
+                lower += v
+        gt_wf = dict(collections.Counter(greater).most_common(topk))
+        lt_wf = dict(collections.Counter(lower).most_common(topk))
+        
+        gt_wf, lt_wf = _filter_same_content(gt_wf, lt_wf)
+#         print("gt", gt_wf)
+#         print("lt", lt_wf)
+        
+        _generate_wc(gt_wf, 'ge_%d_top_%d.png' % (rate_threshold, topk))
+        _generate_wc(lt_wf, 'lt_%d_top_%d.png' % (rate_threshold, topk))
+
+if __name__ == "__main__":
+    DA = DataAnalyse("the_bankers_douban.csv")
+    DA.analyse(4, 100)
